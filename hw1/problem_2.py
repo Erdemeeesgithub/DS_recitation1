6d90b712a33bbec3b5d676fd3d7718d9eac1485c
Erdem <erdem@Phoebes-MacBook-Pro.local>
2026-01-29 19:33:08 +0800

    problem 2 done

--- hw1/problem_2.py
+++ hw1/problem_2.py
@@ -1,17 +1,31 @@
 class Vehicle:
-    # TODO: Please write your code here
-    pass
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        
+    def get_description(self):
+        return f"{self.year} {self.make} {self.model}"
 
 
 class Car(Vehicle):
-    # TODO: Please write your code here
-    pass
+    def __init__(self, make, model, year, doors):
+        super().__init__(make, model, year)
+        self.doors = doors
+        
+    def get_description(self):
+        base = super().get_description()
+        return f"{base}, {self.doors}-door"
 
 
 class Truck(Vehicle):
-    # TODO: Please write your code here
-    pass
+    def __init__(self, make, model, year, payload_capacity):
+        super().__init__(make, model, year)
+        self.payload_capacity = payload_capacity
 
+    def get_description(self):
+        base = super().get_description()
+        return f"{base}, Payload capacity: {self.payload_capacity} tons"
 
 # Do not change or remove the code below this point
 def main():
